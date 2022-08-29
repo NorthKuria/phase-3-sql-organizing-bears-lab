18317c60e5d0200b609ae08b2829df9c508a1b04
NorthKuria <kuri4martin@gmail.com>
2022-08-29 14:21:22 +0300

    create bears table and implement its queries

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 14, 'M', 'CHOCOLATE', 'SWEET', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 15, 'M', 'BEIGE', 'HOT TEMPERED', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 9, 'F', 'WHITE', 'BUBBLY', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 7, 'M', 'BROWN', 'STERN', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 20, 'F', 'BLACK', 'SHY', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 8, 'M', 'BLACK', 'GRUMPY', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 22, 'F', 'WHITE', 'WARM', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 22, 'F', 'WHITE', 'WARM', 1);
+
+
+
